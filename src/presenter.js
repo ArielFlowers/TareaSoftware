88f90c6bb8b8aabb9401525cbdf14b7356949d19
ArielFlowers <arielmakotoflores@gmail.com>
2025-02-13 01:45:18 -0400

    multiplicar agregado

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,27 @@
-import sumar from "./sumador";
+import sumar from "./sumador.js";
+import multiplicar from "./multiplicador.js";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const selectOperation = document.querySelector("#operacion");
+
+// Mapeo de operaciones
+const operaciones = {
+  sumar,
+  multiplicar,
+};
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+  const operacionSeleccionada = selectOperation.value;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  // Ejecutamos la operación seleccionada
+  const resultado = operaciones[operacionSeleccionada](firstNumber, secondNumber);
+
+  div.innerHTML = `<p>Resultado: ${resultado}</p>`;
 });
